98e5873f3f3b78ae3c2bca1c47aca96ee154d6f1
carloselg <72324991+carloselg@users.noreply.github.com>
2021-02-01 22:50:50 -0300

    Delete jsEntradaSalida-01.js

--- jsEntradaSalida-01.js
+++
@@ -1,9 +0,0 @@
-/*
-Al presionar el  botón, 
-se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
-function mostrar()
-{
-	var mensaje = "Esto funciona de Maravilla";
-	alert(mensaje);
-}
-
